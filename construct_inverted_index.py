a4a5df510fdfd1b43b2ba723f6f0c0f479d7227c
bryanas2 <39431416+bryanas2@users.noreply.github.com>
2019-02-15 19:06:46 -0800

    Revert "does exactly the same thing but with added improvements. should run quicker now"
    This reverts commit 54df46c321586475c329370c4b552f875704ea89.

--- construct_inverted_index.py
+++ construct_inverted_index.py
@@ -1,4 +1,4 @@
-from index_loader_saver import save_index
+from index_loader_saver import save_index, load_index
 from pathlib import Path
 from TokenizerModule import Tokenizer
 from html.parser import HTMLParser
@@ -6,69 +6,73 @@ from lxml import html
 import json
 from collections import defaultdict
 
-import datetime
-currentDT = datetime.datetime.now()
-print('time start:', str(currentDT))
-
 
 p = Path('.')  # gives me all the files in the current dir
-# p = [x for x in p.iterdir() if x.is_dir()][2]  # goes to the 'webpages' directory
-p = Path('WEBPAGES_RAW')
+p = [x for x in p.iterdir() if x.is_dir()][2]  # goes to the 'webpages' directory
 url_dict_links = [x for x in p.iterdir() if not x.is_dir()][0]
 p = [x for x in p.iterdir() if x.is_dir()]  # gets all the folders contained in the webpages dir
 
 practice_indexer = defaultdict(list)
 
 with open(url_dict_links, 'r') as file:
-    url_dict = file.read()
+    urldict = file.read()
 
-url_dict = json.loads(url_dict)
+urldict = json.loads(urldict)
 
 
-for sub_file in p:  # for every file in the web pages folder
+# count = 0
+# count_subfolder = 0
 
+for sub_file in p:  # for every file in the webpages folder
+    #print(sub_file)
+    count_file = 0
     for file in sub_file.iterdir():  # go in that file and iterate through the files
+        # print(str(count_subfolder) + '/' + str(count_file))
+        #print(sub_file)
+        #print(file)
 
-        with open(file, 'r', encoding='utf-8') as temp:
-            # temp_readline = str(temp.readlines())
-            tree = html.parse(temp)
-            try:
-                tree = tree.find('.//*')
-            except AssertionError:
-                #  if it comes in here, then that means the file wasnt an html file
-                tree = None
+        # if count < 40000:#just used to debug a certain file, make it equal to some # to debug that file not subfolders though
+        temp = open(file, 'r', encoding='utf-8')
 
-        # tree = html.fromstring(temp_readline).find('.//*')
+        temp_readline = str(temp.readlines())
+
+        tree = html.fromstring(temp_readline).find('.//*')
 
         if tree is not None:  # making sure that the file is html
 
-            strings = tree.xpath('//body')
-            strings.extend(tree.xpath('//title'))
-            strings.extend(tree.xpath('//h1'))
-            strings.extend(tree.xpath('//h2'))
-            strings.extend(tree.xpath('//h3'))
-            strings.extend(tree.xpath('//b'))
-            strings.extend(tree.xpath('//strong'))
-            strings.extend(tree.xpath('//p'))
+            strings = tree.xpath('//p')
 
-            # tokenizer_object = Tokenizer(' '.join([string.text for string in strings
-            #                                        if string.text is not None]))
-            tokenizer_object = Tokenizer(' '.join([string.text for string in strings
-                                                   if string.text is not None]))
+            tokenized_string = Tokenizer(' '.join([string.text for string in strings if string.text is not None]))
 
-            current_file_dict = tokenizer_object.give_dict()
+            current_file_dict = tokenized_string.give_dict()
 
             for x in current_file_dict:
                 practice_indexer[x].append((current_file_dict[x],
-                                            url_dict[str(file).lstrip('WEBPAGES_RAW\\').replace('\\', '/')]))
-        # temp.close()
+                                            urldict[str(file).lstrip('WEBPAGES_RAW\\').replace('\\', '/')]))
+        temp.close()
+        # count += 1
+
+    #     count_file += 1
+    #
+    # count_subfolder += 1
+
+
+
+
+
 
 print('\n', len(practice_indexer))
+print('\n' , count, sep = '')
 
 save_index(practice_indexer)
 
 
+
+
+
+
+
+
 if __name__ == "__main__":
     # file.print_tokens()
-    currentDT = datetime.datetime.now()
-    print('time finish:', str(currentDT))
+    print('\ndone')
